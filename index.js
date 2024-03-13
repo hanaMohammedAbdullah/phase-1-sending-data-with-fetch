334f798fa64a971734760e2ced3c650a2bf09674
hanaMohammedAbdullah <hana.muhammed.abdulla@gmail.com>
2024-03-13 13:44:21 +0300

    Complete Assigmmnet

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData(name, email) {
+  const formData = {
+    name,
+    email,
+  };
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then((response) =>
+      response.json().then((data) => {
+        document.body.innerHTML = data.id;
+      })
+    )
+    .catch((error) => {
+      document.body.innerHTML = error.message;
+    });
+}
+
+let name = "Steve";
+let email = "steve@steve.com";
+const data = submitData(name, email);
+console.log(data);
